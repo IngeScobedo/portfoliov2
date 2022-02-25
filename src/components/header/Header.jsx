53b88b3f4ea973d8a87bc15e064baa92b20c0f1e
AlanDev <ingescobedoalan4@gmail.com>
2022-02-24 22:12:36 -0600

    Add about me component and navbar

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,72 @@
+import '../../index.css'
+import personalImage from '../../images/alanescobedo.jpg'
+
+const Header = () => {
+  return (
+    <header id="home" className="w-full flex justify-center p-10 mt-2 text-center">
+      <div className="flex flex-col items-center bg-opacity-96 backdrop-opacity-90 backdrop-invert p-6 rounded-3xl md:flex-row lg:w-5/6 lg:justify-around">
+        <div className="hidden ml-2 absolute left-0 lg:flex lg:flex-col " >
+            <div className="w-10 h-15 border-t-8 border-red-500 flex justify-center px-2.5 py-2.5 text-center items-center m-3 text-3xl text-gray-600 p-2" >|</div>
+          <button
+            type="button"
+            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 m-2"
+          >
+            <svg
+              className="w-4 h-4"
+              aria-hidden="true"
+              focusable="false"
+              data-prefix="fab"
+              data-icon="github"
+              role="img"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 496 512"
+            >
+              <path
+                fill="currentColor"
+                d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3 .3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5 .3-6.2 2.3zm44.2-1.7c-2.9 .7-4.9 2.6-4.6 4.9 .3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3 .7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3 .3 2.9 2.3 3.9 1.6 1 3.6 .7 4.3-.7 .7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3 .7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3 .7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"
+              ></path>
+            </svg>
+          </button>
+          <button
+            type="button"
+            className="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-2.5 py-2.5 text-center inline-flex items-center justify-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/30 m-2"
+          >
+           <img src="https://img.icons8.com/stickers/100/000000/gmail-new.png" className="w-4 h-4"/>
+          </button>
+          <div className="w-10 h-15 border-t-8 border-red-500 flex justify-center px-2.5 py-2.5 text-center items-center m-3 text-3xl text-gray-600 p-2" >|</div>
+        </div>
+        <div className="lg:ml-10" >
+          <h2 className="text-2xl text-gray-500">Hola yo soy</h2>
+          <h1 className="text-5xl font-bold m-1 text-gray-700">
+            Alan Escobedo
+          </h1>
+          <h3 className="text-2xl text-gray-600 p-2">Javascript Developer</h3>
+          <div className="p-8 w-full flex justify-between">
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:ring-green-200 dark:focus:ring-green-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Descargar CV
+              </span>
+            </button>
+            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:ring-pink-200 dark:focus:ring-pink-800">
+              <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+                Vamos a hablar!
+              </span>
+            </button>
+          </div>
+        </div>
+
+        <div className="flex flex-col justify-center items-center">
+          <div className="flex flex-col items-center ">
+            <img
+              className="mb-3 w-72 h-72 rounded-3xl shadow-lg border p-1"
+              src={personalImage}
+              alt="Bonnie image"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import './testimonials.css'
+
+const Testimonials = () => {
+  return (
+    <div className="testimonials">
+      <h1>Testimonials</h1>
+    </div>
+  )
+}
+export default Testimonials
