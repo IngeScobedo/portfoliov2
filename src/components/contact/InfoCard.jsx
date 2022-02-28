f458358b95608ac4b46b09f9624555397d319cb9
AlanDev <ingescobedoalan4@gmail.com>
2022-02-28 14:22:16 -0600

    Card with contact information

--- src/components/contact/InfoCard.jsx
+++ src/components/contact/InfoCard.jsx
@@ -3,8 +3,8 @@ import { AiOutlineMail } from 'react-icons/ai'
 
 const InfoCard = ({ description, icon }) => {
   return (
-    <div className="w-11/12 bg-gray-700 rounded-xl flex flex-col items-center p-3 my-3">
-      <div className="w-full h-12 flex justify-center items-center">
+    <div className="w-11/12 bg-gray-700 rounded-xl flex flex-col items-center p-3 my-3 sm:w-1/2 text-xs">
+      <div className="w-full h-12 flex justify-center items-center sm:w-1/2">
         {icon === 'whatsapp'
           ? (
           <IoLogoWhatsapp size="large" />
