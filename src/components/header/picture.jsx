5553c276969d12ccc033a113394a671a8943dd9c
AlanDev <ingescobedoalan4@gmail.com>
2022-02-28 16:54:03 -0600

    Handling static files with vite

--- src/components/header/picture.jsx
+++ src/components/header/picture.jsx
@@ -1,10 +1,12 @@
 import personalImage from '../../images/alanescobedo.jpg'
+document.getElementById('personal-image').src = personalImage
 
 const Picture = ({ mb, w, h }) => {
   return (
     <div className="flex flex-col justify-center items-center">
       <div className="flex flex-col items-center justify-center bg-indigo-600 rounded-3xl ">
         <img
+          id="personal-image"
           className={`mb-${mb} w-${w} h-${h} rounded-3xl shadow-lg border p-1 rotate-[10deg]`}
           src={personalImage}
           alt="Alan Escobedo Picture"
